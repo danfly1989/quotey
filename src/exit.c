c15fe1210977dfda2d83765abf497ec4377bead5
flyndann <dannyflynn@gmx.com>
2025-09-17 13:52:49 +0200

    exit slight improvement

--- src/exit.c
+++ src/exit.c
@@ -6,7 +6,7 @@
 /*   By: daflynn <daflynn@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/13 20:05:09 by daflynn           #+#    #+#             */
-/*   Updated: 2025/09/13 20:05:15 by daflynn          ###   ########.fr       */
+/*   Updated: 2025/09/17 14:30:00 by daflynn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,32 @@ char	*append_exit_status(char *res, int status, int *i)
 
 void	ft_exit_numeric_error(char *arg)
 {
-	write(2, "minishell: exit: ", 18);
-	write(2, arg, ft_strlen(arg));
-	write(2, ": numeric argument required\n", 29);
+	ft_putstr_fd("exit: ", 2);
+	if (arg)
+	{
+		ft_putstr_fd(arg, 2);
+		ft_putstr_fd(": ", 2);
+	}
+	ft_putendl_fd("numeric argument required", 2);
+}
+
+int	ft_is_valid_exit_arg(char *str)
+{
+	int	i;
+	int	digits;
+
+	i = 0;
+	digits = 0;
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		digits++;
+		i++;
+	}
+	return (digits > 0);
 }
 
 void	ft_exit(t_dat *data, size_t k)
@@ -36,16 +59,17 @@ void	ft_exit(t_dat *data, size_t k)
 
 	if (data->xln[k + 1] && data->xln[k + 2])
 	{
-		write(2, "minishell: exit: too many arguments\n", 36);
+		ft_putendl_fd("exit: too many arguments", 2);
+		g_last_exit_status = 1;
 		return ;
 	}
 	rl_clear_history();
 	if (data->xln[k + 1] == NULL)
-		ft_cleanup_exit(data, 0);
-	if (ft_is_number(data->xln[k + 1]) == 0)
+		ft_cleanup_exit(data, g_last_exit_status);
+	if (!ft_is_valid_exit_arg(data->xln[k + 1]))
 	{
 		ft_exit_numeric_error(data->xln[k + 1]);
-		ft_cleanup_exit(data, 255);
+		ft_cleanup_exit(data, 2);
 	}
 	status = ft_atoi(data->xln[k + 1]);
 	ft_cleanup_exit(data, status % 256);
@@ -53,8 +77,8 @@ void	ft_exit(t_dat *data, size_t k)
 
 char	*ft_expand_exit_status(t_dat *d, char *token)
 {
-	char	*res;
-	int		i;
+	char *res;
+	int i;
 
 	(void)d;
 	i = 0;
